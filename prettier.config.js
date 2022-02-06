988d3bcb7bc83064e400ce4365e9b8824bf4e167
Makoto Morimoto <Humberto.Morimoto@microsoft.com>
2022-02-05 20:59:56 -0800

    Adding initial setup and theme switch between light and dark. (#1)

---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+// @ts-check
+
+/**
+ * https://prettier.io/docs/en/configuration.html
+ * @type {import('prettier').Options}
+ */
+ module.exports = {
+    printWidth: 120,
+    tabWidth: 2,
+    singleQuote: true,
+    trailingComma: 'all',
+    arrowParens: 'avoid',
+  };
+  
--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import * as React from 'react';
+import { tokens, webDarkTheme, webLightTheme, Button, FluentProvider } from '@fluentui/react-components';
+import { useBoolean } from '@fluentui/react-utilities';
+import { makeStyles } from '@griffel/react';
 import './App.css';
 
+const useAppStyles = makeStyles({
+  app: {
+    alignItems: 'center',
+    backgroundColor: tokens.colorNeutralBackground1,
+    display: 'flex',
+    height: '100%',
+    justifyContent: 'center',
+  },
+});
+
 function App() {
+  const [isDarkTheme, { toggle: toggleDarkTheme }] = useBoolean(false);
+
+  const styles = useAppStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <FluentProvider theme={isDarkTheme ? webDarkTheme : webLightTheme} className={styles.app}>
+      <Button onClick={toggleDarkTheme}>Switch to {isDarkTheme ? 'light theme' : 'dark theme'}</Button>
+    </FluentProvider>
   );
 }
 
