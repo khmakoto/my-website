f2cd48709a10cfcb6fc6f1977203a8e3034fbece
Makoto Morimoto <Humberto.Morimoto@microsoft.com>
2022-03-21 17:28:35 -0600

    Adding sticky top Nav with theme switch. (#3)

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,26 @@
 import * as React from 'react';
-import { tokens, webDarkTheme, webLightTheme, Button, FluentProvider } from '@fluentui/react-components';
+import { makeStyles, tokens, webDarkTheme, webLightTheme, FluentProvider } from '@fluentui/react-components';
 import { useBoolean } from '@fluentui/react-utilities';
-import { makeStyles } from '@griffel/react';
-import './App.css';
+import { IntroSection, Nav } from './components';
 
 const useAppStyles = makeStyles({
   app: {
-    alignItems: 'center',
     backgroundColor: tokens.colorNeutralBackground1,
-    display: 'flex',
     height: '100%',
-    justifyContent: 'center',
   },
 });
 
-function App() {
-  const [isDarkTheme, { toggle: toggleDarkTheme }] = useBoolean(false);
+const App = () => {
+  const [isDarkTheme, { toggle: toggleTheme }] = useBoolean(false);
 
   const styles = useAppStyles();
 
   return (
     <FluentProvider theme={isDarkTheme ? webDarkTheme : webLightTheme} className={styles.app}>
-      <Button onClick={toggleDarkTheme}>Switch to {isDarkTheme ? 'light theme' : 'dark theme'}</Button>
+      <Nav toggleTheme={toggleTheme} />
+      <IntroSection />
     </FluentProvider>
   );
-}
+};
 
 export default App;
